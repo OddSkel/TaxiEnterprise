370d5e933572fc3057829ef6134d70a1c39d036c
fc59809 <fc59809@alunos.fc.ul.pt>
2025-04-21 21:26:30 +0100

    angular arranjado (acho eu)

--- angular-taxi-management/src/app/taxi.service.ts
+++ angular-taxi-management/src/app/taxi.service.ts
@@ -9,7 +9,7 @@ import { MessageService } from './message.service';
 
 @Injectable({ providedIn: 'root' })
 export class TaxiService {
-  private taxisUrl = 'http://localhost:3000/taxis'; // URL to web api
+  private taxisUrl = 'http://localhost:3000/gestor/taxis'; // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
